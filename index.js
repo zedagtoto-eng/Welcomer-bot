367b91e845af0ef29af39351fa097841a8b7ec8d
zedagtoto-eng <zedagtoto@gmail.com>
2026-08-23 03:53:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const {
   Client,
   GatewayIntentBits,
-  EmbedBuilder,
-  ActionRowBuilder,
-  ButtonBuilder,
-  ButtonStyle
+  EmbedBuilder
 } = require("discord.js");
 
 const client = new Client({
@@ -15,7 +12,6 @@ const client = new Client({
 });
 
 const WELCOME_CHANNEL_ID = "1540632238826201099";
-const MARKETPLACE_CHANNEL_ID = "1540632238826201099";
 
 client.once("ready", () => {
   console.log(`✅ ${client.user.tag} is online!`);
@@ -37,32 +33,26 @@ client.on("guildMemberAdd", async (member) => {
   const embed = new EmbedBuilder()
     .setTitle("A New Trader Has Arrived")
     .setDescription(
-      `**Welcome ${member} to adopt me values.**\n\n` +
+      `**Welcome ${member} to ${member.guild.name}.**\n\n` +
+
       `🤝 **Secure Middleman Service**\n` +
       `Protect your trades! Use our trusted MM system for all high-value deals.\n\n` +
+
       `📜 **Server Rules & Terms**\n` +
       `Please review our trading policies before making your first offer.\n\n` +
+
       `🎟️ **Need Help?**\n` +
       `Open a support ticket anytime to talk directly with staff.\n\n` +
+
       `**Member #${member.guild.memberCount} | ID: ${member.id}**`
     )
     .setThumbnail(avatar)
-    .setColor("#5865F2");
-
-  const button = new ButtonBuilder()
-    .setLabel("Check out #marketplace")
-    .setEmoji("➡️")
-    .setStyle(ButtonStyle.Link)
-    .setURL(
-      `https://discord.com/channels/1530545918275883098/${MARKETPLACE_CHANNEL_ID}`
-    );
-
-  const row = new ActionRowBuilder().addComponents(button);
+    .setColor("#8B4513")
+    .setTimestamp();
 
   await channel.send({
     content: `${member}`,
-    embeds: [embed],
-    components: [row]
+    embeds: [embed]
   });
 });
 
